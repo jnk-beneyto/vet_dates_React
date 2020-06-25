b709e06b7fcfc95c33784baa65dace2f827a7601
jnk-beneyto <jnk-beneyto@users.noreply.github.com>
2020-06-25 06:46:51 +0000

    deploy: 209e1e1c5cfe6462c4670a1e07aa5f368203306f

---
+++ precache-manifest.26a9ee8d733c81ecf94974b768e66aa4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec7dae3acf96f2ff9622de7ae7c455f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1eab9d9925af3443ee30",
+    "url": "/static/css/main.05473857.chunk.css"
+  },
+  {
+    "revision": "e4911b6284986cf0b2c0",
+    "url": "/static/js/2.66d68ca1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.66d68ca1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1eab9d9925af3443ee30",
+    "url": "/static/js/main.eedd4301.chunk.js"
+  },
+  {
+    "revision": "6ed635beb37777b8e1e8",
+    "url": "/static/js/runtime-main.499ccc50.js"
+  }
+]);
